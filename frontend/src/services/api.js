5c5a196b23aeae86a2fa24c270b04cd2d6f86eb4
vishal3380y <vishalmeena3380@gmail.com>
2026-09-16 10:01:19 +0530

    connect to backend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -2,3 +2,19 @@ import axios from 'axios';
 const API = axios.create({ baseURL: 'http://localhost:5000' });
 API.interceptors.request.use((config) => { const token=localStorage.getItem('token'); if(token) config.headers.Authorization=`Bearer ${token}`; return config; });
 export default API;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+https://placement-management-backend-axqt.onrender.com
